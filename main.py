77392799aa1ac70df63d837af1e0922cd68250a5
SHJordan <shjordanx@gmail.com>
2024-03-01 14:13:04 -0300

    Count words

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+def count_words(text):
+    """Counts the number of words in a string."""
+
+    words = text.split()
+    return len(words)
+
 def main():
-    """Reads the contents of 'frankenstein.txt' and prints it to the console."""
+    """Reads the contents of 'frankenstein.txt', counts the words, and prints the result."""
 
     with open("books/frankenstein.txt") as f:
         book_contents = f.read()
 
-    print(book_contents)
+    word_count = count_words(book_contents)
+    print("Number of words in the book:", word_count)
 
 if __name__ == "__main__":
     main()
